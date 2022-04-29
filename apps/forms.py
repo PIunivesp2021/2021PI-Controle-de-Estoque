4ab2adfae8f456315cca4eed926580cb94ca3dca
pauloricardoferreira <70301866+pauloricardoferreira@users.noreply.github.com>
2022-04-28 21:16:34 -0300

    Delete apps directory

--- apps/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from app.models import Controle
-
-
-class ControleForm(ModelForm):
-    class Meta:
-        model = Controle
-        fields = ['Empenho', 'Nota_Fiscal', 'Material', 'Obs', 'Data_Envio']
